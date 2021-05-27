01590d86feb6493d1fe5cdab723592ef2006d63a
DungVu0302 <vuvansin1999@gmail.com>
2021-05-27 23:05:27 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: BasicSliverAppbar(),
+    );
+  }
+}
+
+class BasicSliverAppbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            title: Text('SliverAppBar'),
+            backgroundColor: Colors.green,
+            expandedHeight: 200.0,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.asset('assets/forest.jpg', fit: BoxFit.cover),
+            ),
+          ),
+          SliverFixedExtentList(
+            itemExtent: 150.0,
+            delegate: SliverChildListDelegate(
+              [
+                Container(color: Colors.red),
+                Container(color: Colors.purple),
+                Container(color: Colors.green),
+                Container(color: Colors.orange),
+                Container(color: Colors.yellow),
+                Container(color: Colors.pink),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
